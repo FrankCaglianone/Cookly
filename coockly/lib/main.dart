72a6634227fa38fb17c4a62b8444a035a1dabcc1
Francesco Caglianone <francescocaglianone@gmail.com>
2024-09-25 12:11:31 +0200

    refactoring

--- coockly/lib/main.dart
+++ coockly/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:coockly/categories.dart';
+import 'package:coockly/screens/categories.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final theme = ThemeData(
